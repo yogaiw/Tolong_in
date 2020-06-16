0a6a6ae6f2f1f4903eb5e9f2c077c77c9cb39fd8
Yoga <agoy.indra@gmail.com>
2020-06-16 23:33:06 +0700

    ListView

--- app/src/main/java/com/fourthgroup/tolongin/PertolonganPertama.java
+++ app/src/main/java/com/fourthgroup/tolongin/PertolonganPertama.java
@@ -1,14 +1,36 @@
 package com.fourthgroup.tolongin;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.List;
 
 public class PertolonganPertama extends AppCompatActivity {
 
-    CardView cardBHD, cardRJP;
+    ListView listView;
+    String mTitle[] = {
+            "Bantuan Hidup Dasar", "Resus Jantung Paru", "Tenggelam",
+            "Pingsan", "Asma", "Pendarahan"
+    };
+    int images[] = {R.drawable.ic_hospital, R.drawable.ic_hospital, R.drawable.ic_hospital,
+            R.drawable.ic_hospital, R.drawable.ic_hospital, R.drawable.ic_hospital};
+
     LinearLayout minfoBHD, minfoRJP;
 
     @Override
@@ -16,37 +38,66 @@ public class PertolonganPertama extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pertolongan_pertama);
 
-        cardBHD = (CardView) findViewById(R.id.btn_BHD);
-        cardRJP = (CardView) findViewById(R.id.btn_RJP);
+        listView = findViewById(R.id.lisView);
+        minfoBHD = findViewById(R.id.infoBHD);
+        minfoRJP = findViewById(R.id.infoRJP);
 
-        minfoBHD = (LinearLayout) findViewById(R.id.infoBHD);
-        minfoRJP = (LinearLayout) findViewById(R.id.infoRJP);
+        MyAdapter adapter = new MyAdapter(this, mTitle, images);
+        listView.setAdapter(adapter);
 
-        cardBHD.setOnClickListener(new View.OnClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                if (minfoBHD.getVisibility()==View.GONE) {
-                    minfoRJP.setVisibility(View.GONE);
-                    minfoBHD.setVisibility(View.VISIBLE);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0) {
+                    if (minfoBHD.getVisibility() != View.VISIBLE) {
+                        setAllVisibilityGone();
+                        minfoBHD.setVisibility(View.VISIBLE);
+                    } else {
+                        setAllVisibilityGone();
+                    }
                 }
-                else {
-                    /*expandBHD.setVisibility(View.GONE);*/
-                    minfoBHD.setVisibility(View.GONE);
+
+                if(position == 1) {
+                    if (minfoRJP.getVisibility() != View.VISIBLE) {
+                        setAllVisibilityGone();
+                        minfoRJP.setVisibility(View.VISIBLE);
+                    } else {
+                        setAllVisibilityGone();
+                    }
                 }
             }
         });
+    }
 
-        cardRJP.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (minfoRJP.getVisibility() == View.GONE) {
-                    minfoBHD.setVisibility(View.GONE);
-                    minfoRJP.setVisibility(View.VISIBLE);
-                }
-                else {
-                    minfoRJP.setVisibility(View.GONE);
-                }
-            }
-        });
+    private void setAllVisibilityGone() {
+        minfoBHD.setVisibility(View.GONE);
+        minfoRJP.setVisibility(View.GONE);
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+        Context context;
+        String rTitle[];
+        int rImgs[];
+
+        MyAdapter(Context c, String title[], int imgs[]) {
+            super(c, R.layout.row_pertolongan, R.id.textView1, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rImgs = imgs;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row_pertolongan, parent, false);
+            ImageView images = row.findViewById(R.id.image1);
+            TextView myTitle = row.findViewById(R.id.textView1);
+
+            images.setImageResource(rImgs[position]);
+            myTitle.setText(rTitle[position]);
+
+            return row;
+        }
     }
 }
